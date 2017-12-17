4f63a6880d41f07fdcef30ca0b5c386f2a0fb197
gkozub <glib.kozub@gmail.com>
2017-12-17 19:35:37 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <Person name='Vova' years='29' />
+      </div>
+    );
+  }
+}
+
+export default App;
