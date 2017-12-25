a8c78f40db0eb0130e0a05e1dc0b91c995d5a233
gkozub <glib.kozub@gmail.com>
2017-12-25 11:38:21 +0200

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Vova', age: 29},
+      {name: 'Petro', age: 28},
+    ],
+    showPersons: false,
+  };
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: event.target.value, age: 29},
+        {name: 'Petro', age: 28},
+      ]
+    })
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  };
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px'
+    }
+
     return (
       <div className='App'>
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <Person name='Vova' years='29' />
+        <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        {
+          this.state.showPersons ?
+            <div>
+              <Person name={this.state.persons[0].name} years={this.state.persons[0].age}
+                      change={this.nameChangeHandler}/>
+              <Person name={this.state.persons[1].name} years={this.state.persons[1].age}/>
+            </div>
+            : null
+        }
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Person from './Person.css';
 
 const person = (props) => {
-  return <p>I'm a {props.name} and I'm {props.years} years old!</p>
+  return (
+    <div className='Person'>
+      <p>I'm a {props.name} and I'm {props.years} years old!</p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.change} value={props.name} />
+    </div>
+  );
 };
 
 export default person;
