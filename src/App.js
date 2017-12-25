7a434af314b8a3dde860942c0ec4f3f7404ba6ba
gkozub <glib.kozub@gmail.com>
2017-12-25 11:46:22 +0200

    Conditional rendering in a variable

--- src/App.js
+++ src/App.js
@@ -34,19 +34,23 @@ class App extends Component {
       padding: '8px'
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person name={this.state.persons[0].name} years={this.state.persons[0].age}
+                  change={this.nameChangeHandler}/>
+          <Person name={this.state.persons[1].name} years={this.state.persons[1].age}/>
+        </div>
+      )
+    }
+
     return (
       <div className='App'>
         <h1>Hi, I'm a React App</h1>
         <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-        {
-          this.state.showPersons ?
-            <div>
-              <Person name={this.state.persons[0].name} years={this.state.persons[0].age}
-                      change={this.nameChangeHandler}/>
-              <Person name={this.state.persons[1].name} years={this.state.persons[1].age}/>
-            </div>
-            : null
-        }
+        {persons}
       </div>
     );
   }
