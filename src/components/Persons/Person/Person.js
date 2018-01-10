ea7eec43722d553c791dc6c28ce7ee9794e51825
gkozub <gkoz@diceus.com>
2018-01-10 17:02:19 +0200

    Log component creation hooks -> fix

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -4,18 +4,20 @@ import classes from './Person.css';
 class Person extends Component {
   constructor (props) {
     super(props);
-    console.log('[Persons.js] Inside Constructor', props);
+    console.log('[Person.js] Inside Constructor', props);
   }
 
   componentWillMount () {
-    console.log('[Persons.js] Inside componentWillMount')
+    console.log('[Person.js] Inside componentWillMount')
   }
 
   componentDidMount () {
-    console.log('[Persons.js] Inside componentDidMount')
+    console.log('[Person.js] Inside componentDidMount')
   }
 
   render () {
+    console.log('[Person.js] Inside render');
+
     return (
       <div className={classes.Person}>
         <p onClick={this.props.click}>I'm a {this.props.name} and I'm {this.props.years} years old!</p>
