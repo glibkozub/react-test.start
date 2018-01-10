240dfd1d874b40a8d9570066907966f815021d55
gkozub <gkoz@diceus.com>
2018-01-10 18:20:27 +0200

    Add update hooks

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -2,17 +2,34 @@ import React, {Component} from 'react';
 import Person from './Person/Person';
 
 class Persons extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
     console.log('[Persons.js] Inside Constructor', props);
   }
 
-  componentWillMount () {
+  componentWillMount() {
     console.log('[Persons.js] Inside componentWillMount')
   }
 
-  componentDidMount () {
+  componentDidMount() {
     console.log('[Persons.js] Inside componentDidMount')
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('[Update Persons.js] Inside componentWillReceiveProps', nextProps)
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Update Persons.js] Inside shouldComponentUpdate', nextProps, nextState);
+    return nextProps.persons !== this.props.persons;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[Update Persons.js] Inside componentWillUpdate', nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log('[Update Persons.js] Inside componentDidUpdate');
   }
 
   render () {
